6abece6a293f2d849af2ee3e31361e41ab2e126e
Scott Hickmann <scotthickmann21@gmail.com>
2023-05-02 02:10:57 -0700

    Initial Slack app

---
+++ README.md
@@ -0,0 +1,1 @@
+# Slack Messages Cleaner
---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import { createEventAdapter } from '@slack/events-api';
+import { WebClient } from '@slack/web-api';
+import fs from 'fs/promises';
+
+const SLACK_TOKEN = process.env.SLACK_TOKEN;
+if (!SLACK_TOKEN) {
+  throw new Error('No SLACK_TOKEN provided');
+}
+const SLACK_SIGNING_SECRET = process.env.SLACK_SIGNING_SECRET;
+if (!SLACK_SIGNING_SECRET) {
+  throw new Error('No SLACK_SIGNING_SECRET provided');
+}
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : undefined;
+if (!PORT || isNaN(PORT)) {
+  throw new Error('No PORT provided');
+}
+
+const slackWeb = new WebClient(SLACK_TOKEN);
+const slackEvents = createEventAdapter(SLACK_SIGNING_SECRET);
+
+interface Message {
+  type: 'message';
+  subtype?: string;
+  ts: string;
+  user: string;
+  text: string;
+  channel: string;
+  event_ts: string;
+  channel_type: string;
+}
+
+const MESSAGES_TO_DELETE = new Set([
+  'channel_join',
+  'channel_name',
+  'channel_purpose'
+]);
+
+slackEvents.on('message', async (message: Message) => {
+  if (message.subtype && MESSAGES_TO_DELETE.has(message.subtype)) {
+    const log = `${message.channel}: ${message.text}`;
+    console.log(log);
+    await fs.appendFile('messages.txt', `${log}\n`);
+    const response = await slackWeb.chat.delete({
+      channel: message.channel,
+      ts: message.ts,
+      as_user: true
+    });
+    if (!response.ok) {
+      console.error(response);
+    }
+  }
+});
+
+(async () => {
+  // Start the built-in server
+  await slackEvents.start(PORT);
+
+  // Log a message when the server is ready
+  console.log(`Listening for events on ${PORT}`);
+})();
